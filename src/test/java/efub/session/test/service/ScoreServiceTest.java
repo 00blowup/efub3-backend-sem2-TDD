399b370dc74eba1314c3f5d987725f6ca9ac5ed6
Haeun Noh <00blowup@gmail.com>
2023-09-25 20:47:04 +0900

    메소드명 및 변수명 수정

--- src/test/java/efub/session/test/service/ScoreServiceTest.java
+++ src/test/java/efub/session/test/service/ScoreServiceTest.java
@@ -4,47 +4,58 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
 public class ScoreServiceTest {
 
-    @Autowired
-    private MockMvc mockMvc;
-
     @InjectMocks
     private ScoreService scoreService;
 
-    // 일반적인 테스트 케이스를 하나 작성
+    // 일반적인 테스트 케이스(높은 점수, A)를 작성
     @Test
-    public void scoreServiceTest() {
+    public void doScore_GivenHighScore_ReturnsGradeA() throws Exception {
         // given
-        Long value1 = 87L;
+        final Long HIGH_SCORE = 95L;
+        final String GRADE_A = "A";
 
         // when, then
-        assertThat(scoreService.doScore(value1)).isEqualTo("B");
+        assertThat(scoreService.doScore(HIGH_SCORE)).isEqualTo(GRADE_A);
     }
 
-    // 비정상 데이터에 대한 테스트 케이스를 작성
+    // 일반적인 테스트 케이스(낮은 점수, F)를 작성
     @Test
-    public void abnormalScoreTest1() {
+    public void doScore_GivenLowScore_ReturnsGradeF() throws Exception {
         // given
-        Long value2 = -2L;
+        final Long LOW_SCORE = 20L;
+        final String GRADE_F = "F";
 
-        //when, then
-        assertThat(scoreService.doScore(value2)).isEqualTo("Insert a number from 0 to 100");
+        // when, then
+        assertThat(scoreService.doScore(LOW_SCORE)).isEqualTo(GRADE_F);
     }
 
-    @Test
-    public void abnormalScoreTest2() {
-        // given
-        Long value3 = 200L;
 
-        //when, then
-        assertThat(scoreService.doScore(value3)).isEqualTo("Insert a number from 0 to 100");
+    // 비정상 데이터(음수)에 대한 테스트 케이스를 작성
+    @Test
+    public void doScore_GivenMinusScore_ReturnsWarningPhrase() throws Exception {
+        // given
+        final Long MINUS_SCORE = -2L;
+        final String WARNING_PHRASE = "Insert a number from 0 to 100";
+
+        // when, then
+        assertThat(scoreService.doScore(MINUS_SCORE)).isEqualTo(WARNING_PHRASE);
+    }
+
+    // 비정상 데이터(100점을 초과)에 대한 테스트 케이스를 작성
+    @Test
+    public void doScore_GivenTooHighScore_ReturnsWarningPhrase() throws Exception {
+        // given
+        final Long TOO_HIGH_SCORE = 200L;
+        final String WARNING_PHRASE = "Insert a number from 0 to 100";
+
+        // when, then
+        assertThat(scoreService.doScore(TOO_HIGH_SCORE)).isEqualTo(WARNING_PHRASE);
     }
 
 }
